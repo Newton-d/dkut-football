7845d6652d52135a7e74d4731142bd109acd59bd
start.vaadin.com <no-reply@vaadin.com>
2022-06-12 04:51:59 +0000

    Generated project

---
+++ src/main/java/com/dkutfootball/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.dkutfootball.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/dkutfootball/application/views/uploadresults/UploadResultsView.java
@@ -0,0 +1,33 @@
+package com.dkutfootball.application.views.uploadresults;
+
+import com.dkutfootball.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import javax.annotation.security.RolesAllowed;
+
+@PageTitle("Upload Results")
+@Route(value = "uploadresults", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+public class UploadResultsView extends VerticalLayout {
+
+    public UploadResultsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
